4f0c29accf6e0f79aaaf8c81b8247444a5472a00
Tomi <tbanjoko25@gmail.com>
2026-01-29 15:20:05 -0500

    Home screen added, ui changed to login ui, added global vars file

---
+++ vars.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+
+
+import tkinter  as tk
+from PIL import ImageTk, Image
+from Backend.GetInfo import getInfo
+root = tk.Tk()
+root.title("T&X Fitness")
+# Set the window size to 500 pixels wide by 850 high
+root.geometry("500x850")
