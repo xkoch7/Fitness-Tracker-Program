7d6d2557cfe1f1c691f78262dd327ed4dbc3e354
Tomi <tbanjoko25@gmail.com>
2026-02-24 10:34:43 -0500

    added logout button to homescreen

--- frontend/HomeScreen.py
+++ frontend/HomeScreen.py
@@ -15,19 +15,26 @@ def openSettingsScreen(root,tk):
     # dont put pack on same line makes the var a NoneType
     underConst.grid(column=1, row=7)
     root.after(2000, lambda: underConst.grid_forget())  
-     
+    
+def logUserOut():
+    from vars import changeScreen, email
+    email =''
+    changeScreen(0) 
+    
 #function to set up all buttons and labels actually adding something to the visuals and functionality
 # returns list of widgets that get deleted on screen change
 def setup(root,tk) -> list:
     welcomeLabel=tk.Label(root, text="Welcome to T&X Fitness", font=("Helvetica", 16))
     welcomeLabel.grid(column=1, row=0, pady=20, padx=100)
     trackBtn = tk.Button(root, text="Track Workout", width=20, height=2,command=lambda: openTrackingScreen())
-    trackBtn.grid(column=1, row=2, pady=10,padx=100)
+    trackBtn.grid(column=1, row=2, pady=10, padx=100)
     historyBtn = tk.Button(root, text="View History", width=20, height=2, command=lambda: openHistoryScreen(root,tk))
-    historyBtn.grid(column=1, row=3, pady=10,padx=100)
-    settingsBtn = tk.Button(root, text="Settings", width=20, height=2,command=lambda: openSettingsScreen(root,tk))
-    settingsBtn.grid(column=1, row=4, pady=10,padx=100)
-    return [trackBtn, historyBtn, settingsBtn]
+    historyBtn.grid(column=1, row=3, pady=10, padx=100)
+    settingsBtn = tk.Button(root, text="Settings", width=20, height=2, command=lambda: openSettingsScreen(root,tk))
+    settingsBtn.grid(column=1, row=4, pady=10, padx=100)
+    logOutBtn = tk.Button(root, text="Log Out", width=20, height=2, command = lambda:logUserOut())
+    logOutBtn.grid(column=1, row=5, pady=10, padx=100)
+    return [trackBtn, historyBtn, settingsBtn, logOutBtn]
     
 
 if __name__ == "__main__":
