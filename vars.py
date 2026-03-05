1ec56322484dd1dbb1f52b19fd8f883e8c28b0ca
Tomi <tbanjoko25@gmail.com>
2026-03-05 14:20:53 -0500

    CI test 2

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 
 import tkinter  as tk
-screen=1
+
 root = tk.Tk()
 root.title("T&X Fitness")
 # Set the window size to 500 pixels wide by 850 high
@@ -8,9 +8,10 @@ root.geometry("500x850")
 widgets=[]
 email=""
 index=0
-
+screen=1
 def changeScreen(newScreen: int) -> None:
-    global screen, widgets,root
+    
+    global screen
     screen = newScreen  # Move to the new screen
     for widget in widgets:
         widget.grid_forget()
