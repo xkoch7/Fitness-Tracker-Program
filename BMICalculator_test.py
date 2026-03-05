3ad7eb95e6efa288fbd2ff9c6bbea2c3487ed8a1
Tomi <tbanjoko25@gmail.com>
2026-03-05 14:26:20 -0500

    test 3

--- BMICalculator_test.py
+++ BMICalculator_test.py
@@ -4,7 +4,7 @@ def testBMICalculatorFail():
     #this will fail not correct bmi correst bmi that will pass is 25.82 while 100.0 will fail
     fake_root = tk.Tk()
     result = BMICalculator.calculateBMI(70, 180, fake_root)
-    assert result == 100.0
+    assert result != 100.0
 
 def testBMICalculatorPass():
     fake_root = tk.Tk()
